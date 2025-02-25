4d3439f77d7d172e47b46ffc21384435f6b5234a
DannyV1992 <dava01cr@gmail.com>
2025-02-25 10:45:11 -0600

    Archivos agregados

---
+++ notas_de_clase/algoritmos_ordenamiento/sorting_merge_quick/test-sort.py
@@ -0,0 +1,21 @@
+import sorting2 as sort
+
+lista = [99,22,19,12,78,34,90,40,57,14,55,32,77,11,4,192,3]
+""""
+print(lista)
+ordenada = sort.mergesort(lista)
+print(ordenada)
+"""
+print(lista)
+ordenada = sort.mergeSortNR(lista)
+print(ordenada)
+
+lista = [99,22,19,12,78,34,90,40,57,14,55,32,77,11,4,192,3]
+#print(lista)
+#sort.quickSort(lista,0, len(lista)-1)
+#print(lista)
+
+lista = [99,22,19,12,78,34,90,40,57,14,55,32,77,11,4,192,3]
+#print(lista)
+#sort.quickSortIterative(lista, 0, len(lista)-1)
+#print(lista)
