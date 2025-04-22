870dc53482866827d1f4c6d01af66a20cf6cf85a
DannyV1992 <dava01cr@gmail.com>
2025-04-21 20:04:47 -0600

    Practica final

---
+++ practica_final/Ejercicio_1/grafo_matriz.py
@@ -0,0 +1,23 @@
+class grafo_matriz:
+    def __init__(self, node_qty, is_directed):
+        self.__matriz = []
+        for _ in range(0,node_qty):
+            self.__matriz.append([0] * node_qty)
+        self.__is_directed = is_directed
+    
+    def show_graph_matriz(self):
+        for row in self.__matriz:
+            print(row)
+    
+    def add_edge(self, src, dst):
+        self.__matriz[src][dst] = 1
+        self.__matriz[dst][src] = 1
+        if not self.__is_directed:
+            self.__matriz[dst][src] = 1
+
+    def list_adyacency(self, node):
+        adyacency_list = []
+        for col in range(0, len(self.__matriz)):
+            if self.__matriz[node][col] == 1:
+                adyacency_list.append(col)
+        return adyacency_list
